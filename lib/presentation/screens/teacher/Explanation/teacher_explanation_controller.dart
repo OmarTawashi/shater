79dedb5589a35b9af66d59cd03bb83cb9a234622
Omar al Tawashi <omaromar510@gmail.com>
2024-04-18 13:47:31 +0200

    dashborad teacher_controller.dart screen

---
+++ lib/presentation/screens/teacher/Explanation/teacher_explanation_controller.dart
@@ -0,0 +1,99 @@
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:shater/core/controller/base_controller.dart';
+import 'package:shater/core/controller/shared_prefrences.dart';
+import 'package:shater/core/network/api_client.dart';
+import 'package:shater/data/model/subject_model.dart';
+import 'package:shater/data/repository/dashboard_repository_remote.dart';
+import 'package:shater/domain/usecase/dachboard_usercase_imp.dart';
+
+import '../../../../data/model/class_model.dart';
+import '../../../../data/model/course_learning_model.dart';
+import '../../../../data/repository/public_repository_remote.dart';
+import '../../../../domain/usecase/public_usecase_imp.dart';
+import '../../student/dashBord/controller/dashboard_controller.dart';
+import '../teacher dashborad/controller/teacher_dashboard_controller.dart';
+
+class TeacherExplanationController  extends BaseController{
+  DashBaoardUseCaseImp? _dashBaoardUseCaseImp;
+  PublicUseCaseImp? _publicUseCaseImp;
+
+  List<CourseLearningModel> _subjects = [];
+  List<CourseLearningModel> get subjects => _subjects;
+
+  List<Classes> _classes = [];
+  List<Classes> get classes => _classes;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _dashBaoardUseCaseImp =
+        DashBaoardUseCaseImp(DashBoardRepositoryRemote(ApiClient()));
+    _publicUseCaseImp = PublicUseCaseImp(PublicRepositoryRemote(ApiClient()));
+
+    getClassesStudent();
+    fetchTeacherCoursesLesson();
+  }
+  Classes? getClassForItem(int? id){
+    return this.classes?.firstWhere((element) => element.id == '$id');
+  }
+
+  void getClassesStudent() async {
+    final dashController = Get.find<TeacherDashBoardController>();
+    final cityId = dashController.user?.city?.id;
+    final schoolId = dashController.user?.school?.id;
+    final dataRegister = SharedPrefs.dataRegister;
+
+   if(dataRegister?.country?.first != null){
+     _classes = dataRegister?.country?.first.classes ?? [];
+     this.populateData();
+     update();
+   }
+   else{
+     await _publicUseCaseImp
+         ?.fetchClassStudent(cityId ?? -1, schoolId ?? -1)
+         .then((value) {
+       value?.fold((l) {
+         updateViewType(ViewType.error);
+       }, (r) {
+         if (r == null) {
+           updateViewType(ViewType.empty);
+         } else {
+           updateViewType(ViewType.success);
+           _classes = r.country?.first.classes ?? [];
+           this.populateData();
+         }
+       });
+
+       update();
+     });
+   }
+  }
+  void fetchTeacherCoursesLesson() async {
+    final dashController = Get.find<TeacherDashBoardController>();
+
+    updateViewType(ViewType.loading);
+    await _dashBaoardUseCaseImp?.fetchTeacherCoursesLesson(dashController.level?.id ?? -1).then((value) {
+      value?.fold((l) {
+        updateViewType(ViewType.error);
+      }, (s) {
+        if (s.isEmpty) {
+          updateViewType(ViewType.empty);
+        } else {
+          updateViewType(ViewType.success);
+          _subjects = s;
+          this.populateData();
+        }
+      });
+      update();
+    });
+  }
+  void populateData(){
+    var isEmpty = (this.classes ?? []).isEmpty;
+    _subjects.forEach((element) {
+      if(!isEmpty){
+        element.classes = this.getClassForItem(element.classId);
+      }
+    });
+  }
+}
---
+++ lib/presentation/screens/teacher/practice/view/practice_item_subject.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:shater/data/model/class_model.dart';
+import 'package:shater/presentation/screens/base/cashed_network_image_widget.dart';
+
+import '../../../../../util/color.dart';
+import '../../../../../util/dimensions.dart';
+import '../../../base/text_custom.dart';
+
+class PracticeItemSubject extends StatelessWidget {
+  final Function()? onTap;
+  final String? textSubject;
+  final String? imageUrl;
+  final int? questionCount;
+
+  const PracticeItemSubject({
+    super.key,
+    this.onTap,
+    this.textSubject,
+    this.imageUrl,
+    this.questionCount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Container(
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 3,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+                child: Container(
+                  clipBehavior: Clip.hardEdge,
+                  height: 100,
+                  width: 100,
+                  padding: const EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: CachedNetworkImageWidget(
+                    imageUrl: imageUrl ?? '',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: Dimensions.paddingSize16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CustomText(
+                      text: textSubject ?? "",
+                      color: Colors.white,
+                      fontSize: Dimensions.fontSize18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    SizedBox(
+                      height: Dimensions.paddingSize20,
+                    ),
+                    CustomText(
+                      text: '$questionCount' '\t' + 'تدريب'.tr,
+                      color: Color.fromRGBO(198, 235, 245, 1),
+                      fontSize: Dimensions.fontSize16,
+                      fontWeight: FontWeight.bold,
+                    )
+                    ,
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: onTap,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 12),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(32),
+                      color: Colors.white),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomText(
+                        text: 'درب',
+                        color: COLORS.primaryColor,
+                        textAlign: TextAlign.start,
+                        fontSize: Dimensions.fontSize14,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      SizedBox(
+                        width: Dimensions.paddingSize10,
+                      ),
+                      Icon(
+                        Icons.arrow_forward_ios_sharp,
+                        color: COLORS.primaryColor,
+                        size: 16,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Divider(
+            color: COLORS.primaryColor.withOpacity(0.15),
+            height: 18,
+            thickness: 1.5,
+            endIndent: 80.w,
+            indent: 8,
+          )
+        ],
+      ),
+    );
+  }
+}
